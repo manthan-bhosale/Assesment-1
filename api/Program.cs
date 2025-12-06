98e5a5732774f4dca9029cb8fa0e1f25602c8668
Manthan Bhosale <manthanbhosale7@outlook.com>
2025-12-06 23:07:05 +0530

    feat: add CORS policy to allow React app requests

--- api/Program.cs
+++ api/Program.cs
@@ -13,7 +13,15 @@ namespace AvalphaTechnologies.CommissionCalculator
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReactApp", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod(); 
+                });
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -22,6 +30,8 @@ namespace AvalphaTechnologies.CommissionCalculator
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors("AllowReactApp");
 
             app.UseHttpsRedirection();
 
